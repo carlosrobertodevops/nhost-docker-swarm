6560be9364e39fe03459cda6b2d32fae1ba2f37d
Viktor Andersen <vikfand@gmail.com>
2025-01-17 17:59:55 +0100

    Update postgres deploy script

--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -5,24 +5,25 @@ docker compose -f ./postgres/docker-compose.yml convert --no-path-resolution | g
 
 # Replace quoted integers in the `published` port with unquoted integers
 sed -i 's/published: "\(.*\)"/published: \1/' ./postgres/docker-compose.canonical.yml
+sed -i 's/size: "\(.*\)"/size: \1/' ./postgres/docker-compose.canonical.yml
 
 # Sync the files to the remote server using rsync
-rsync -avz --delete ./postgres/ root@your.server.ip:/root/postgres/
+rsync -avz --delete ./postgres/ root@199.199.199.199:/root/postgres/
 
 # Log in to the remote server and deploy the stack using docker
-ssh root@your.server.ip << EOF
+ssh root@199.199.199.199 << EOF
   # Navigate to the folder where the docker-compose.yml is located
   cd /root/postgres/
 
   # Deploy the stack using docker stack deploy
-  docker stack deploy -c docker-compose.canonical.yml postgres-stack --resolve-image always
+  docker stack deploy -c docker-compose.canonical.yml postgres-stack --resolve-image always --detach
 
   # Find the running PgBouncer container
   PGB_CONTAINER_ID=\$(docker ps -q --filter "name=postgres-stack_pgbouncer")
 
   if [ -n "\$PGB_CONTAINER_ID" ]; then
     # Send SIGHUP to PgBouncer to reload the configuration (removed -t for TTY)
-    docker exec -i \$PGB_CONTAINER_ID kill -SIGHUP 1
+    docker exec -i \$PGB_CONTAINER_ID pgbouncer -R /etc/pgbouncer/pgbouncer.ini
     echo "PgBouncer reloaded"
   else
     echo "PgBouncer container not found"
