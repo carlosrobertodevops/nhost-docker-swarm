0ead0237093e52f63cef46dd56dbbffa4410f503
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-05-21 19:35:22 -0300

    Ajustes dos .shs

--- nhost/deploy.sh
+++ nhost/deploy.sh
@@ -13,14 +13,14 @@ fi
 docker compose -f ./nhost/docker-compose.yml convert --no-path-resolution | grep -v '^name'  > ./nhost/docker-compose.canonical.yml
 
 # Sync the files to the remote server using rsync
-rsync -avz --delete ./nhost/ root@199.199.199.199:/root/nhost-$APP_NAME/
+# rsync -avz --delete ./nhost/ root@199.199.199.199:/root/nhost-$APP_NAME/
 
 
-# Log in to the remote server and deploy the stack using docker
-ssh root@199.199.199.199 << EOF
-  # Navigate to the folder where the docker-compose.yml is located
-  cd /root/nhost-$APP_NAME/
+# # Log in to the remote server and deploy the stack using docker
+# ssh root@199.199.199.199 << EOF
+#   # Navigate to the folder where the docker-compose.yml is located
+#   cd /root/nhost-$APP_NAME/
 
-  # Deploy the stack using docker stack deploy
-  docker stack deploy -c docker-compose.canonical.yml nhost-$APP_NAME-stack --detach --resolve-image always
-EOF
+#   # Deploy the stack using docker stack deploy
+#   docker stack deploy -c docker-compose.canonical.yml nhost-$APP_NAME-stack --detach --resolve-image always
+# EOF
--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -7,25 +7,25 @@ docker compose -f ./postgres/docker-compose.yml convert --no-path-resolution | g
 sed -i 's/published: "\(.*\)"/published: \1/' ./postgres/docker-compose.canonical.yml
 sed -i 's/size: "\(.*\)"/size: \1/' ./postgres/docker-compose.canonical.yml
 
-# Sync the files to the remote server using rsync
-rsync -avz --delete ./postgres/ root@199.199.199.199:/root/postgres/
+# # Sync the files to the remote server using rsync
+# rsync -avz --delete ./postgres/ root@199.199.199.199:/root/postgres/
 
-# Log in to the remote server and deploy the stack using docker
-ssh root@199.199.199.199 << EOF
-  # Navigate to the folder where the docker-compose.yml is located
-  cd /root/postgres/
+# # Log in to the remote server and deploy the stack using docker
+# ssh root@199.199.199.199 << EOF
+#   # Navigate to the folder where the docker-compose.yml is located
+#   cd /root/postgres/
 
-  # Deploy the stack using docker stack deploy
-  docker stack deploy -c docker-compose.canonical.yml postgres-stack --resolve-image always --detach
+#   # Deploy the stack using docker stack deploy
+#   docker stack deploy -c docker-compose.canonical.yml postgres-stack --resolve-image always --detach
 
-  # Find the running PgBouncer container
-  PGB_CONTAINER_ID=\$(docker ps -q --filter "name=postgres-stack_pgbouncer")
+#   # Find the running PgBouncer container
+#   PGB_CONTAINER_ID=\$(docker ps -q --filter "name=postgres-stack_pgbouncer")
 
-  if [ -n "\$PGB_CONTAINER_ID" ]; then
-    # Send SIGHUP to PgBouncer to reload the configuration (removed -t for TTY)
-    docker exec -i \$PGB_CONTAINER_ID pgbouncer -R /etc/pgbouncer/pgbouncer.ini
-    echo "PgBouncer reloaded"
-  else
-    echo "PgBouncer container not found"
-  fi
-EOF
+#   if [ -n "\$PGB_CONTAINER_ID" ]; then
+#     # Send SIGHUP to PgBouncer to reload the configuration (removed -t for TTY)
+#     docker exec -i \$PGB_CONTAINER_ID pgbouncer -R /etc/pgbouncer/pgbouncer.ini
+#     echo "PgBouncer reloaded"
+#   else
+#     echo "PgBouncer container not found"
+#   fi
+# EOF
