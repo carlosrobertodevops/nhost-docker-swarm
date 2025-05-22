dc00778a7814f36907de29c654b7ed272b501d45
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-05-22 19:25:59 -0300

    ajustes

--- nhost/deploy.sh
+++ nhost/deploy.sh
@@ -12,15 +12,15 @@ fi
 # Convert docker-compose.yml to canonical form to insert env variables
 docker compose -f ./nhost/docker-compose.yml convert --no-path-resolution | grep -v '^name'  > ./nhost/docker-compose.canonical.yml
 
-# Sync the files to the remote server using rsync
-# rsync -avz --delete ./nhost/ root@199.199.199.199:/root/nhost-$APP_NAME/
+Sync the files to the remote server using rsync
+rsync -avz --delete ./nhost/ root@91.99.135.154:/root/nhost-$APP_NAME/
 
 
-# # Log in to the remote server and deploy the stack using docker
-# ssh root@199.199.199.199 << EOF
-#   # Navigate to the folder where the docker-compose.yml is located
-#   cd /root/nhost-$APP_NAME/
+# Log in to the remote server and deploy the stack using docker
+ssh root@91.99.135.154 << EOF
+  # Navigate to the folder where the docker-compose.yml is located
+  cd /root/nhost-$APP_NAME/
 
-#   # Deploy the stack using docker stack deploy
-#   docker stack deploy -c ./nhost/docker-compose.canonical.yml nhost-$APP_NAME-stack --detach --resolve-image always
-# EOF
+  # Deploy the stack using docker stack deploy
+  docker stack deploy -c ./nhost/docker-compose.canonical.yml nhost-$APP_NAME-stack --detach --resolve-image always
+EOF
--- portainer/deploy.sh
+++ portainer/deploy.sh
@@ -6,18 +6,18 @@ docker compose -f ./portainer/docker-compose.yml convert --no-path-resolution | 
 # Replace quoted integers in the `published` port with unquoted integers
 sed -i 's/published: "\(.*\)"/published: \1/' ./portainer/docker-compose.canonical.yml
 
-# # Sync the files to the remote server using rsync
-# rsync -avz --delete ./portainer/ root@199.199.199.199:/root/portainer/
+# Sync the files to the remote server using rsync
+rsync -avz --delete ./portainer/ root@191.99.135.154:/root/portainer/
 
-# # Log in to the remote server and deploy the stack using docker
-# ssh root@199.199.199.199 << 'EOF'
-#   # Navigate to the folder where the docker-compose.canonical.yml is located
-#   cd /root/portainer/
+# Log in to the remote server and deploy the stack using docker
+ssh root@91.99.135.154 << 'EOF'
+  # Navigate to the folder where the docker-compose.canonical.yml is located
+  cd /root/portainer/
 
-#   # Loki user needs to be the owner of these folders
-#   chown -R 10001:10001 ./loki_data
-#   chmod -R 755 ./loki_data
+  # Loki user needs to be the owner of these folders
+  chown -R 10001:10001 ./loki_data
+  chmod -R 755 ./loki_data
 
-#   # Deploy the stack using docker stack deploy --resolve-image always 
-#   docker stack deploy -c ./portainer/docker-compose.canonical.yml portainer-stack --resolve-image always --detach
-# EOF
+  # Deploy the stack using docker stack deploy --resolve-image always
+  docker stack deploy -c ./portainer/docker-compose.canonical.yml portainer-stack --resolve-image always --detach
+EOF
