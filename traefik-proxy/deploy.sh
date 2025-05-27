4ae3cdbc622751f97af6a2c13eed5f09aabb6bcf
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-05-27 10:24:56 -0300

    AJUSTES

--- traefik-proxy/deploy.sh
+++ traefik-proxy/deploy.sh
@@ -15,5 +15,26 @@ docker compose -f docker-compose.yml convert --no-path-resolution | grep -v '^na
 #   cd /root/nhost-docker-swarm/traefik-proxy
 
 #   # Deploy the stack using docker stack deploy --resolve-image always
+  docker stack deploy -c docker-compose.canonical.yml traefik-stack --resolve-image always --detach
+# EOF
+
+
+# ORIGINAL
+
+# # Convert docker-compose.yml to canonical form to insert env variables
+# docker compose -f ./traefik-proxy/docker-compose.yml convert --no-path-resolution | grep -v '^name'  > ./traefik-proxy/docker-compose.canonical.yml
+
+# # Replace quoted integers in the `published` port with unquoted integers
+# sed -i 's/published: "\(.*\)"/published: \1/' ./traefik-proxy/docker-compose.canonical.yml
+
+# # Sync the files to the remote server using rsync
+# rsync -avz --delete ./traefik-proxy/ root@199.199.199.199:/root/traefik-proxy/
+
+# # Log in to the remote server and deploy the stack using docker
+# ssh root@199.199.199.199 << 'EOF'
+#   # Navigate to the folder where the docker-compose.canonical.yml is located
+#   cd /root/traefik-proxy/
+
+#   # Deploy the stack using docker stack deploy --resolve-image always
 #   docker stack deploy -c docker-compose.canonical.yml traefik-stack --resolve-image always --detach
 # EOF
