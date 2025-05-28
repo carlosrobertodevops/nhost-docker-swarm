7c7bfa0d8261584e4a5c6f2467e92d4f477c8a83
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-05-28 15:53:27 -0300

    Ajustes

--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Convert docker-compose.yml to canonical form to insert env variables
-# docker compose -f docker-compose.yml convert --no-path-resolution | grep -v '^name'  > docker-compose.canonical.yml
+docker compose -f docker-compose.yml convert --no-path-resolution | grep -v '^name'  > docker-compose.canonical.yml
 
 # Replace quoted integers in the `published` port with unquoted integers
-# sed -i 's/published: "\(.*\)"/published: \1/' docker-compose.canonical.yml
-# sed -i 's/size: "\(.*\)"/size: \1/' docker-compose.canonical.yml
+sed -i 's/published: "\(.*\)"/published: \1/' docker-compose.canonical.yml
+sed -i 's/size: "\(.*\)"/size: \1/' docker-compose.canonical.yml
 
 # Sync the files to the remote server using rsync
 # rsync -avz --delete ./postgres/ root@91.99.128.127:/root/postgres/
